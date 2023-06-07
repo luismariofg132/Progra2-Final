89a9b8a2627189ce4b1229f033e3b6ce720253bb
Keiro18 <113016173+Keiro18@users.noreply.github.com>
2023-06-07 22:56:50 +0000

    Se agregan estilos para jugadores y torneo

--- Front/Scripts/jugadores.js
+++ Front/Scripts/jugadores.js
@@ -2,6 +2,31 @@ const tableGA = document.getElementById('tableGA');
 const tableGB = document.getElementById('tableGB');
 const url = 'http://127.0.0.1:3000/api/data/';
 
+var modal = document.getElementById("myModal");
+
+// Obtener el botón que abre el modal
+var btn = document.querySelector("button");
+
+// Obtener el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Función para abrir el modal
+function openModal() {
+    modal.style.display = "block";
+}
+
+// Función para cerrar el modal
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// Cerrar el modal si se hace clic fuera del contenido del modal
+window.onclick = function (event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     const res = await fetch(url);
     const data = await res.json();
